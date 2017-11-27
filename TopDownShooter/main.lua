bbeaf5b1fbc72b09b01b293975c8cc3c809f7f24
Graggor <edwin@immortale.net>
2017-11-27 21:44:13 +0100

    Added zombies, they follow player, when player hit > all zombies despawn

--- TopDownShooter/main.lua
+++ TopDownShooter/main.lua
@@ -9,6 +9,8 @@ function love.load()
   player.x = 200
   player.y = 200
   player.speed = 180
+
+  zombies = {}
 end
 
 function love.update(dt)
@@ -25,13 +27,53 @@ function love.update(dt)
     player.x = player.x + player.speed * dt
   end
 
-end
+  for i,z in ipairs(zombies) do
+    -- Makes zombie move
+    z.x = z.x + math.cos(zombie_player_angle(z)) * z.speed * dt
+    z.y = z.y + math.sin(zombie_player_angle(z)) * z.speed * dt
+
+    -- Zombie / Player collision check
+    if distanceBetween(z.x, z.y, player.x, player.y) < 30 then
+      -- Game over, remove zombies
+      for i,z in ipairs(zombies) do
+        zombies[i] = nil
+      end
+    end
+  end
+end -- love.update
 
 function love.draw()
   love.graphics.draw(sprites.background, 0, 0)
   love.graphics.draw(sprites.player, player.x, player.y, player_mouse_angle(), nil, nil, sprites.player:getWidth() / 2, sprites.player:getHeight() / 2)
+
+  for i,z in ipairs(zombies) do
+    love.graphics.draw(sprites.zombie, z.x, z.y, zombie_player_angle(z), nil, nil, sprites.zombie:getWidth() /2, sprites.zombie:getHeight() / 2)
+  end
 end
 
 function player_mouse_angle()
   return math.atan2(player.y - love.mouse.getY(), player.x - love.mouse.getX()) + math.pi
+end
+
+function zombie_player_angle(enemy)
+  return math.atan2(player.y - enemy.y, player.x - enemy.x)
+end
+
+function spawnZombie()
+  zombie = {}
+  zombie.x = math.random(0, love.graphics.getWidth())
+  zombie.y = math.random(0, love.graphics.getHeight())
+  zombie.speed = 100
+
+  table.insert(zombies, zombie)
+end
+
+function love.keypressed(key, scancode, isrepeat)
+  if key == "space" then
+    spawnZombie()
+  end
+end
+
+function distanceBetween(x1, y1, x2, y2)
+  return math. sqrt((y2 - y1)^2 + (x2 - x1)^2)
 end
