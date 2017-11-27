6829cf97ba8efbd0c5441ab1872b925d886f7e09
Graggor <edwin@immortale.net>
2017-11-27 17:52:14 +0100

    Button init, score / timer vars included

--- button_clicker/main.lua
+++ button_clicker/main.lua
@@ -1,11 +1,18 @@
 function love.load()
-  number = 0
+  button = {}
+  button.x = 200
+  button.y = 200
+  button.size = 50
+
+  score = 0
+  timer = 0
 end
 
 function love.update(dt)
-  number = number + 1
+
 end
 
 function love.draw()
-  love.graphics.print(number)
+  love.graphics.setColor(255, 0, 255, 255)
+  love.graphics.circle("fill", button.x, button.y, button.size)
 end
