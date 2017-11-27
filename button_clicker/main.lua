9cbb2ec6119b98b046a321f8b5041dbc2aa35b0d
Graggor <edwin@immortale.net>
2017-11-27 20:12:07 +0100

    Added timer, menu. Antialiasing 8x msaa

--- button_clicker/main.lua
+++ button_clicker/main.lua
@@ -6,31 +6,67 @@ function love.load()
 
   score = 0
   timer = 10
+  gameState = 1
+  newGameTimer = 0
 
   myFont = love.graphics.newFont(40)
 end
 
 function love.update(dt)
+  if gameState == 2 then
+    if timer > 0 then
+      timer = timer - dt
+    end
 
+    if timer < 0 then
+      timer = 0
+      newGameTimer = 4
+      gameState = 1
+    end
+  end
+  if gameState == 1 then
+    if newGameTimer > 0 then
+      newGameTimer = newGameTimer - dt
+    end
+
+    if newGameTimer < 0 then
+      newGameTimer = 0
+    end
+  end
 end
 
 function love.draw()
-  love.graphics.setColor(255, 0, 255, 255)
-  love.graphics.circle("fill", button.x, button.y, button.size)
+  if gameState == 2 then
+    love.graphics.setColor(255, 0, 255, 255)
+    love.graphics.circle("fill", button.x, button.y, button.size)
+  end
 
   love.graphics.setFont(myFont)
 
   love.graphics.setColor(200, 200, 200, 255)
-  love.graphics.print(score)
+  love.graphics.print("Score: " .. score)
+  love.graphics.print("Time: " .. math.ceil(timer), 220, 0)
+
+  if gameState == 1 and newGameTimer == 0 then
+    love.graphics.printf("Click anywhere to begin!", 0, love.graphics.getHeight()/2, love.graphics.getWidth(), "center")
+  elseif gameState == 1 and newGameTimer > 0 then
+    love.graphics.printf("New game loading: " .. math.ceil(newGameTimer) .. "...", 0, love.graphics.getHeight()/2, love.graphics.getWidth(), "center")
+  end
 end
 
 function love.mousepressed(x, y, b, isTouch)
-  if b == 1 then
+  if b == 1 and gameState == 2 then
     if distanceBetween(button.x, button.y, x, y) < button.size then
       score = score + 1
       button.x = math.random(button.size, love.graphics.getWidth() - button.size)
       button.y = math.random(button.size, love.graphics.getHeight() - button.size)
     end
+  end
+
+  if gameState == 1 and newGameTimer == 0 then
+    gameState = 2
+    timer = 10
+    score = 0
   end
 end
 
