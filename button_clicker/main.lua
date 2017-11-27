84779dd92d75cecb09ecc5c0551ee1df8c3fff6e
Graggor <edwin@immortale.net>
2017-11-27 18:11:36 +0100

    Distance formula & clicking in the circle to increase score.

--- button_clicker/main.lua
+++ button_clicker/main.lua
@@ -6,6 +6,8 @@ function love.load()
 
   score = 0
   timer = 0
+
+  myFont = love.graphics.newFont(40)
 end
 
 function love.update(dt)
@@ -15,4 +17,21 @@ end
 function love.draw()
   love.graphics.setColor(255, 0, 255, 255)
   love.graphics.circle("fill", button.x, button.y, button.size)
+
+  love.graphics.setFont(myFont)
+
+  love.graphics.setColor(200, 200, 200, 255)
+  love.graphics.print(score)
+end
+
+function love.mousepressed(x, y, b, isTouch)
+  if b == 1 then
+    if distanceBetween(button.x, button.y, x, y) < button.size then
+      score = score + 1
+    end
+  end
+end
+
+function distanceBetween(x1, y1, x2, y2)
+  return math.sqrt((y2 - y1)^2 + (x2 - x1)^2)
 end
